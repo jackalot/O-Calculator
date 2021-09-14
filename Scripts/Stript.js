f4d720979e0b8f99e70ba6c69b44cbf6fc778e2d
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-14 07:45:45 -0400

    added 4 functions, add. multiply, subtract, divide

---
+++ Scripts/Stript.js
@@ -0,0 +1,12 @@
+function add (a, b) {
+    return a + b;
+}
+function subtract (c, d) {
+    return c - d;
+}
+function multiply (e, f) {
+    return e * f;
+}
+function divide (g, h) {
+    return g / h;
+}
